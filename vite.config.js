6c9f6d47b56fbc75a6005ac8e566ce4655fd6683
Dmitri Herdt <github@herdt.online>
2026-03-05 21:01:49 +0100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    // Allows the server to be accessible outside the container
+    host: '0.0.0.0',
+    port: 5173,
+    // Ensures HMR works through the VS Code port forward
+    hmr: {
+      clientPort: 5173,
+    },
+    // Required if you are on Windows/WSL and file changes aren't detected
+    watch: {
+      usePolling: true,
+    }
+  },
+})
