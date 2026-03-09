0b5f9c2728646ce330471c7184e5d37a7a31e1b3
Dmitri Herdt <github@herdt.online>
2026-03-09 13:36:57 +0000

    adding GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/learning_react",
   plugins: [react()],
   server: {
     // Allows the server to be accessible outside the container
